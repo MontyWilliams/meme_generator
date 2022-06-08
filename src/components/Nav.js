259b44fe4629a31b092146540350445cf596670f
Montywilliams <3650@holbertonschool.com>
2022-06-08 12:23:59 -0600

    nav portion completed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import '../styles/nav_styles.css'
+import '../styles/nav_styles.css';
+import logo from '../images/Logo.png';
 
 export default function Nav() {
   return(
     <div className="nav">
       <div className="nav_con">
-        <h1>yo</h1>
+        <div className="nav_con_L">
+        <img src={logo} alt="" />
+        <span>Meme Generator</span>
+        </div>
+        <div className="nav_con_R">
+        <span>React Course - Project 3</span>
+        </div>
       </div>
 
     </div>
