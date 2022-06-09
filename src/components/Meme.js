719535e2efc0d9387551edd4e5b2fcfb58bd8778
Montywilliams <3650@holbertonschool.com>
2022-06-08 21:58:00 -0600

    added meme Array

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import '../styles/meme_styles.css';
+import Memes from '../memesData';
 
 export default function Meme() {
+
+  function getMemeImage() {
+    const memesArray = Memes.data.memes;
+    console.log(memesArray)
+}
+
+
   return(
     <div className="meme_con">
      
       <input type="text" placeholder="yea" className="meme_in_1"/>
       <input type="text" placeholder="yea yea" className="meme_in_2"/>
 
-        <button className="meme_btn">Get yo memes her Bru 🖼</button> 
+        <button onClick={getMemeImage} className="meme_btn">Get yo memes here Bru 🖼</button> 
     </div>
   )
 } 
