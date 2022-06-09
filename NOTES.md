60655c8767f709f24172da7428e0917916a4e138
Montywilliams <3650@holbertonschool.com>
2022-06-09 11:26:24 -0600

    added state to updat memeimage at randam and a div to display it with. this state dosnt require callback function because it constantly updates and dosnt depend on previos value

--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,4 @@
+# State and Props
 1. How would you describe the concept of "state"?
 A way for React to remember saved values from within a component.
 This is similar to declaring variables from within a component,
@@ -18,3 +19,21 @@ when React re-renders the component).
 
 4. What does "immutable" mean? Are props immutable? Is state immutable?
 Unchanging. Props are immutable. State is mutable.
+
+# Updating State
+
+1. You have 2 options for what you can pass in to a 
+state setter function (e.g. `setCount`). What are they?
+   a. New value of state (setCount(42))
+   b. Callback function - whatever the callback function 
+   returns === new value of state
+
+2. When would you want to pass the first option (from answer
+   above) to the state setter function?
+Whenever you don't need the previous value of state to determine
+what the new value of state should be.
+
+
+3. When would you want to pass the second option (from answer
+   above) to the state setter function?
+Whenever you DO need the previous value to determine the new value
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,37 +1,48 @@
-import React from 'react';
-import '../styles/meme_styles.css';
-import Memes from '../memesData';
+import React from "react";
+import "../styles/meme_styles.css";
+import Memes from "../memesData";
 
 
 export default function Meme() {
 
+  const [memeImage, setmemeImage] = React.useState("");
+
   function getRandonMeme() {
     const memesArray = Memes.data.memes;
     // Get a random number based on the Array length
-    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
     // we destructure the object at the index and pull value by key
-    const {url} = memesArray[randomNumber]
-    console.log(url)
+    const { url } = memesArray[randomNumber];
+    setmemeImage(url)
   }
-  function printRandomMeme(){
+  function printRandomMeme() {
     const memesArray = Memes.data.memes;
-    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
     for (let i = 0; i < randomNumber; i++) {
-     const payDay = memesArray.map(thing => <p key={thing}>{thing}</p>)
+      const payDay = memesArray.map(thing =>
+        <p key={thing}>
+          {thing}
+        </p>
+      );
     }
- 
-
   }
 
-  return(
-  <div>
-    <div className="meme_con">
-      <input type="text" placeholder="yea" className="meme_in_1"/>
-      <input type="text" placeholder="yea yea" className="meme_in_2"/>
+  return (
+    <div>
+      <div className="meme_con">
+        <input type="text" placeholder="yea" className="meme_in_1" />
+        <input type="text" placeholder="yea yea" className="meme_in_2" />
 
-        <button onClick={printRandomMeme} className="meme_btn">Print a meme Bru 🖼</button> 
-        <button onClick={getRandonMeme} className="meme_btn">Random button</button> 
+        <button onClick={printRandomMeme} className="meme_btn">
+          Print a meme Bru 🖼
+        </button>
+        <button onClick={getRandonMeme} className="meme_btn">
+          Random button
+        </button>
+      </div>
+      <div className="meme_img_con">
+        <img className="meme_img_con" src={memeImage} alt="" />
+      </div>
     </div>
-    </div>
-  )
-} 
+  );
+}
