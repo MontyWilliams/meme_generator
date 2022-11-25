d6f7b707147ad56007a98f9a0363ed767577afe3
Montywilliams <3650@holbertonschool.com>
2022-11-25 10:25:57 -0600

    the project is almost compete. using html to image to save a screenshot of the meme ill

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { createRef, useRef, useState } from 'react'
 import "../styles/meme_styles.css";
-// import Memes from "../memesData";
+import {render} from 'react-dom';
+import * as htmlToImage from 'html-to-image';
+
+
+
 
 
 export default function Meme(props) {
+
+  
+  const ref = React.createRef(null)
+
+  const getImage = () => {
+    htmlToImage.toPng(ref.current)
+      .then(function(dataUrl){
+        var img = new Image()
+        img.src = dataUrl
+        document.body.appendChild(img)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    console.log('getImage')
+    
+  }
 
   const [meme, setMeme] = React.useState({
     topText: "",
@@ -13,6 +34,7 @@ export default function Meme(props) {
 
   const [Memes, setAllMemes] = React.useState([]);
 
+  
   React.useEffect(() => {
     fetch("https://api.imgflip.com/get_memes")
     .then(res => res.json())
@@ -27,42 +49,57 @@ export default function Meme(props) {
     setMeme(e => {
       return{
         ...e,
-        randomImage: url
-        
+        randomImage: url  
       }
-    })
-   
+    })  
   }
   
   function handleChange(event){
     const {name, value} = event.target
     setMeme(e => ({
       ...e,
-      [name]: value
-      
+      [name]: value 
     }))
   }
  
-  
   return (
     <div  className="wrap">
       <div className="meme_con">
-        
-        <input type="text" placeholder="Top text" className="meme_in_1" name="topText" value={meme.topText} onChange={handleChange}/>
-        <input type="text" placeholder="Bottom text" className="meme_in_2" name="bottomText" value={meme.bottomText} onChange={handleChange}/>
+    
+        <input className="meme_in_1" onChange={handleChange}
+          maxLength="52"
+          type="text"
+          placeholder="Top text"
+          name="topText"
+          value={meme.topText}
+        />
+
+        <input type="text" className="meme_in_2" onChange={handleChange}
+          maxLength="52"
+          placeholder="Bottom text"
+          name="bottomText"
+          value={meme.bottomText}
+          />
+
         <button onClick={getRandomMeme} className="meme_btn">
           Random button 🖼
-        </button>
-        
+        </button>       
       </div>
-      <div className="memeDisplay">
-        <img className="meme_img_con" src={meme.randomImage} alt="" />
-          <div >
-            <h2 className=" top">{meme.topText}</h2> 
-            <h2 className=" bottom" >{meme.bottomText}</h2> 
+      <div className="memeDisplay" ref={ref} >
+        <img className="meme_img_con" src={meme.randomImage} alt=""/>
+          <div className="topText">
+            <h2 >{meme.topText}</h2> 
+          </div>
+          <div className="bottomText">
+            <h2 >{meme.bottomText}</h2> 
           </div>
       </div>
-    
+
+      <button onClick={getImage} className="meme_btn">
+          Random button 🖼
+        </button>
+
+          
     </div>
   );
 }
