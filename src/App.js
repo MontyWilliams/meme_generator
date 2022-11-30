44f397b3107956f74f249f90ae124b337a9cf3f2
Montywilliams <3650@holbertonschool.com>
2022-11-30 15:48:13 -0600

    added background image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Nav  from "./components/Nav";
 import Meme from "./components/Meme";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
+import Town from "./Tulsa.mp4";
+
 
 
 
@@ -10,9 +12,12 @@ import './styles/styles.css';
 function App() {
   return (
     <div className="App">
+       <video className="TTown" autoPlay loop muted  width="100%" height="100%" >
+    <source src={Town} type="video/mp4" />
+    </video>
      <Nav />
      <Meme />
-     {/* <Footer /> */}
+     <Footer />
     </div>
   );
 }
